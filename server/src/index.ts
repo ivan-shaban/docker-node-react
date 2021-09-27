9605c173502549673206f4ba1f8cfe8160385833
Ivan Shaban <i_shaban@wargaming.net>
2021-09-27 11:57:42 +0300

    update server with base routing

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,69 @@
-import './pre-start'; // Must be the first import
-import app from '@server';
-import logger from '@shared/Logger';
+// Must be the first import
+import './pre-start'
+import express from 'express'
 
+import mysql from 'mysql2'
+
+import cors from 'cors'
+import logger from '@shared/Logger'
+
+const db = mysql.createPool({
+    host: 'mysql_db', // the host name MYSQL_DATABASE: node_mysql
+    user: 'MYSQL_USER', // database user MYSQL_USER: MYSQL_USER
+    password: 'MYSQL_PASSWORD', // database user password MYSQL_PASSWORD: MYSQL_PASSWORD
+    database: 'books', // database name MYSQL_HOST_IP: mysql_db
+})
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+app.get('/', (req, res) => {
+    res.send('Hi There')
+})
+
+//get all of the books in the database
+app.get('/get', (req, res) => {
+    const SelectQuery = ' SELECT * FROM books_reviews'
+    db.query(SelectQuery, (err, result) => {
+        res.send(result)
+    })
+})
+
+// add a book to the database
+app.post('/insert', (req, res) => {
+    const bookName = req.body.setBookName
+    const bookReview = req.body.setReview
+    const InsertQuery = 'INSERT INTO books_reviews (book_name, book_review) VALUES (?, ?)'
+    db.query(InsertQuery, [bookName, bookReview], (err, result) => {
+        console.log(result)
+    })
+})
+
+// delete a book from the database
+app.delete('/delete/:bookId', (req, res) => {
+    const bookId = req.params.bookId
+    const DeleteQuery = 'DELETE FROM books_reviews WHERE id = ?'
+    db.query(DeleteQuery, bookId, (err, result) => {
+        if (err) console.log(err)
+    })
+})
+
+// update a book review
+app.put('/update/:bookId', (req, res) => {
+    const bookReview = req.body.reviewUpdate
+    const bookId = req.params.bookId
+    const UpdateQuery = 'UPDATE books_reviews SET book_review = ? WHERE id = ?'
+    db.query(UpdateQuery, [bookReview, bookId], (err, result) => {
+        if (err) console.log(err)
+    })
+})
 
 // Start the server
-const port = Number(process.env.PORT || 3000);
+// const port = Number(process.env.PORT || 3000)
+const port = 3001
 app.listen(port, () => {
     logger.info('Express server started on port: ' + port);
-});
+})
